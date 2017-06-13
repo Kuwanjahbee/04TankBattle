9097fa2510c6086dc1315d7b8646782138012821
DESKTOP-U41Q900\Spencer <spencer.james.barrett@gmail.com>
2017-06-12 19:10:33 -0500

    Lecture 120 Using Virtual and Override

--- TankBattle/Source/TankBattle/Private/TankPlayerController.cpp
+++ TankBattle/Source/TankBattle/Private/TankPlayerController.cpp
@@ -4,7 +4,19 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not possing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
