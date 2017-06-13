af24a4600a5ac91a8a1797a945045423e7f29218
DESKTOP-U41Q900\Spencer <spencer.james.barrett@gmail.com>
2017-06-13 05:43:33 -0500

    Lecture 124 Creating an AI controller

---
+++ TankBattle/Source/TankBattle/Private/TankAIController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankBattle.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController Not possing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController is possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
